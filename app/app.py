dca78aaf56de3b7ab210fd42a9d170a794928562
JairEsc <p.escobar816@gmail.com>
2025-03-28 10:57:51 -0600

    codigo comentado para agregar logo. Aún no funcional

--- app/app.py
+++ app/app.py
@@ -171,7 +171,25 @@ content = html.Div(
                   ],
         style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block", 'opacity': 1,'z-index':'3'},
         className=''
-    )],
+    ),
+    # html.Div(
+    #     children=[
+    #         html.Img(
+    #             src="./assets/logo lab.png",  
+    #             style={
+    #                 "width": "10wh",   # Tamaño del ícono
+    #                 "height": "10vh",  # Tamaño del ícono
+    #             }
+    #         )
+    #     ],
+    #     style={ # Estilos CSS aplicados al Div
+    #         "position": "absolute", # El elemento no ocupa espacio en el diseño habitual. Esto significa que otros elementos actúan como si el elemento "absoluto" no existiera.
+    #         "bottom": "2vh",   # Posicionamiento desde el borde inferior
+    #         "left": "5wh",     # Posicionamiento desde el borde izquierdo
+    #         "z-index": "1000"   # Asegura que esté sobre otros elementos
+    #     }
+    # )
+    ],
     style={'width': '100%', 'height': '50vh'}
 )
 interior_alt_content=dcc.Graph(id='interior-alt-content',figure={},style={'height':'91.5vh', 'background-color':'lightgray'},
