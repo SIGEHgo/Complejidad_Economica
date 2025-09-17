0adbb2166f965185f1678e2e232cf16e3281c290
SIGEHgo <sigeh@hidalgo.gob.mx>
2025-09-17 14:24:55 -0600

    Se actualizo el dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,13 @@
-FROM python:3.8-slim-buster
+FROM python:3.11-slim-buster
 
 
 
 RUN apt-get update
 RUN apt-get install nano
 
-RUN mkdir wd
-WORKDIR wd
 COPY app/requirements.txt .
 RUN pip3 install -r requirements.txt
 
 COPY app/ ./
+EXPOSE 80
 CMD [ "gunicorn", "--workers=1", "--threads=2", "-b", "0.0.0.0:80", "--timeout", "600", "--preload", "app:server"]
--- app/app.py
+++ app/app.py
@@ -43,7 +43,7 @@ gdf_shapefile=gdf_shapefile.reset_index()
 df_estatal['NOM_MUN'] = gdf_shapefile['NOM_MUN']
 
 map_default=auxiliarLeafltet.generateMapFromElection(lista_de_opciones_personal[-1],df_estatal,gdf_shapefile)
-df_industrial=pd.read_csv("Datos/CSVs/Balassa_Modificado_Historico/Balassa_Mod_Nivel_Municipio_por_Grupos_2024B.csv")
+df_industrial=pd.read_csv("Datos/CSVs/Balassa_Modificado_Historico/Balassa_Mod_Nivel_Municipio_por_Grupos_2025A.csv")
 print(df_industrial)
 server = Flask(__name__)
 
